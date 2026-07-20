37929f42f05a502da1ea60f98977c832a5542c9a
jungla <jeanmensa@gmail.com>
2026-07-20 14:56:06 +0300

    updated navigation

--- home.py
+++ home.py
@@ -21,7 +21,7 @@ st.logo("img/WCS-logo_only.png", icon_image="img/WCS-logo_only.png", size="large
 bony_page= st.Page("bony_fishes.py", title="Bony Fishes", icon="🐠")
 shark_page = st.Page("sharks_and_rays.py", title="Sharks and Rays", icon="🦈")
 protected_species_page = st.Page("protected_species.py", title="Protected Species List", icon="🐋")
-restoration_page = st.Page("restoration.py", title="Coral Reef Restoration", icon="🐋")
+restoration_page = st.Page("restoration.py", title="Coral Reef Restoration", icon="🌊")
 
-pg = st.navigation([bony_page, shark_page, protected_species_page], position="sidebar", expanded=True)
+pg = st.navigation([bony_page, shark_page, protected_species_page, restoration_page], position="sidebar", expanded=True)
 pg.run()
